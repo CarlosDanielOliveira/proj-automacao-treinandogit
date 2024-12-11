8951d1b3d5273962e38e54e52ccc84737a14fb95
Carlos Daniel Oliveira <carlosdanielbarbosadeoliveira@gmail.com>
2024-12-10 22:39:52 -0300

    debug_aprendendo

--- appteste.py
+++ appteste.py
@@ -2,3 +2,19 @@ print('Testando versionamento com GIT')
 
 x = input('Qual seu nome?')
 print(x)
+
+
+
+
+#Código utilizado na aula para debugar (acharmos o erro):
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante #erro aqui int + str 
+    print(total)
+
+calcular_preco_combo(30, 'vinte reais')
+
+print('Programa finalizado')
